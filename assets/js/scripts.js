b6de574f523f3472bb6586a3875f82aee7c5f6ef
Saul Navarrov <sinavarrov@gmail.com>
2017-01-07 14:30:49 -0500

    agregando control de navegacion

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,35 @@
-$(function () {
+var urlPath = window.location.pathname; // identificador de la pagina.
 
-  });
+/**************************************************************************************************
+*                                                                                                 *
+*                                          Menu function                                          *
+*                                                                                                 *
+***************************************************************************************************/
+// Funcion para activar el menu y ver cual esta seleccionado
+if(location.pathname){
+  switch (location.pathname) {
+    case '/':                  setChangeActiveMenu('das'); break;
+    case '/history':           setChangeActiveMenu('his'); break;
+    case '/consultas':         setChangeActiveMenu('con'); break;
+    case '/permisos':          setChangeActiveMenu('per'); break;
+    case '/taxis':             setChangeActiveMenu('tax'); break;
+    case '/zonas':             setChangeActiveMenu('zon'); break;
+    case '/profile':           setChangeActiveMenu('pro'); break;
+    case '/notificaciones':    setChangeActiveMenu('not'); break;
+    case '/ajustes/users':     setChangeActiveMenu('aUse', 'aju'); break;
+    case '/ajustes/logs':      setChangeActiveMenu('aLog','aju'); break;
+    default: break;
+  }
+}
+
+/**
+ * setChangeActiveMenu description
+ * @param {[Strign]} z [El id del li del menu]
+ * @param {[Strign]} x [El id del li del menu, en caso de que tenga un padre el li ajustes > logs]
+ */
+function setChangeActiveMenu(z,x){
+  var a = $(`#${z}`); // Busca el identificador que se ha seleccionado
+  var b = $(`#${x}`); // Busca el identificador Padre selecionado ajustes > generales
+  a.addClass('active'); // añade la clase
+  b.addClass('active');
+}
